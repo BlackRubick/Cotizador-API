20b921dbdbaccaf3794cb310cf68d45f593b938e
BlackRubick <blackrubick14@gmail.com>
2025-09-22 13:24:03 -0600

    casi todo listo

--- middleware/cors.js
+++ middleware/cors.js
@@ -2,8 +2,8 @@ const cors = require('cors');
 
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
-    ? ['https://cotizador.com']
-    : ['http://52.14.169.93:5000', 'http://localhost:3001'],
+    ? ['http://52.14.169.93', 'https://cotizador.com']  
+    : ['http://52.14.169.93:5000', 'http://localhost:3001', 'http://52.14.169.93'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
